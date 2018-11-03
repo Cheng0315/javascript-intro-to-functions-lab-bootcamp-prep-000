33cb18995bf42a1aee62457385682f909a639207
Cheng Vue <cvu3@ucsc.edu>
2018-11-03 20:11:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,18 @@ function logShout(str) {
   str = str.toUpperCase();
   console.log(str);
 }
+
+function logWhisper(str) {
+  str = str.toLowerCase();
+  console.log(str);
+}
+
+function sayHiToGrandma(str) {
+  if (str === str.toLowerCase()) {
+    return "I can't hear you!";
+  } else if (str === str.toUpperCase()) {
+    return "YES INDEED!";
+  } else if (str === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
